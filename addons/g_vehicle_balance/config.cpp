a8900b8974ed56311ed1e0e0ce36287481ca602c
Ryan <ryan@mcnamee.it>
2026-06-07 11:54:34 -0500

    Initial Vehicle Balance

--- addons/g_vehicle_balance/config.cpp
+++ addons/g_vehicle_balance/config.cpp
@@ -7,6 +7,18 @@ class CfgPatches
       requiredAddons[] =
       {
          "OPTRE_Vehicles",
+         "OPTRE_Vehicles_Air_Falcon",
+         "OPTRE_Vehicles_Hornet",
+         "OPTRE_Vehicles_Sparrowhawk",
+         "OPTRE_Vehicles_Pelican",
+         "OPTRE_Vehicles_Bison",
+         "OPTRE_Vehicles_Scorpion",
+         "OPTRE_Vehicles_M808B2",
+         "OPTRE_Vehicles_Misc",
+         "OPTRE_Vehicles_Elephant",
+         "Scythe",
+         "Lance",
+         "OPTRE_ACE_Compat",
          "DMNS_Vehicles_M511_Springbok",
          "DMNS_Vehicles_M808B",
          "DMNS_Armour"
@@ -16,79 +28,141 @@ class CfgPatches
 
 class CfgVehicles
 {
-   class OPTRE_M413_base
+   /*
+      TCP M41 HEAT reference: TCP_M_102mm_HEAT has hit = 95 and a Titan AT
+      penetrator submunition. Global armor values below are scaled around the
+      requested number of HEAT hits to kill or disable the vehicle.
+   */
+
+   class Heli_Light_01_base_F;
+   class Helicopter_Base_H;
+   class APC_Wheeled_01_base_F;
+   class B_MBT_01_cannon_F;
+   class O_Truck_02_transport_F;
+   class AAA_System_01_base_F;
+   class Tank_F;
+
+   class OPTRE_falcon_base: Heli_Light_01_base_F
    {
-      armor = 420;
+      armor = 220;              // Falcon: 2 HEAT
+      armorStructural = 3;
+      damageResistance = 0.00555;
+   };
+
+   class OPTRE_Hornet_base: Helicopter_Base_H
+   {
+      armor = 330;              // Hornet: 1.5 HEAT; tested 1/1 at 220
+      armorStructural = 3;
+      damageResistance = 0.00555;
+   };
+
+   class OPTRE_AV22_Sparrowhawk_Base: Helicopter_Base_H
+   {
+      armor = 285;              // Sparrowhawk: 3 HEAT
+      armorStructural = 3;
+      damageResistance = 0.00555;
+   };
+
+   class OPTRE_Pelican_F: Helicopter_Base_H
+   {
+      armor = 245;              // Pelican: 4 HEAT; tested 5/5 at 305
+      armorStructural = 3;
+      damageResistance = 0.00555;
+   };
+
+   class OPTRE_M413_base: APC_Wheeled_01_base_F
+   {
+      armor = 760;              // Bison / M412 IFV: 2 HEAT; tested 1/1 at 380
       armorStructural = 3;
       damageResistance = 0.005;
 
       class HitPoints
       {
-         class HitFuel
-         {
-            armor = 1.0;
-            name = "palivo";
-            passThrough = 0.15;
-            explosionShielding = 0.4;
-            minimalHit = 0.05;
-         };
-
-         class HitEngine
-         {
-            armor = 0.8;
-            name = "engine";
-            passThrough = 0.2;
-            explosionShielding = 0.4;
-            minimalHit = 0.05;
-         };
-
          class HitBody
          {
-            armor = 1.6;
-            name = "zbytek";
-            visual = "zbytek";
-            passThrough = 0.65;
-            explosionShielding = 0.25;
-            minimalHit = 0.1;
-            radius = 0.3;
+            armor = 4.5;
          };
       };
    };
 
+   class OPTRE_M808B_base: B_MBT_01_cannon_F
+   {
+      armor = 760;              // Scorpion: 4 HEAT; tested 2/2 at 380
+      armorStructural = 3.25;
+      damageResistance = 0.0055;
+      crewExplosionProtection = 0.65;
+   };
+
+   class OPTRE_M808B2: B_MBT_01_cannon_F
+   {
+      armor = 760;              // M808B2 / Sun Devil: 4 HEAT; tested 2/2 at 380
+      armorStructural = 3.25;
+      damageResistance = 0.0055;
+      crewExplosionProtection = 0.65;
+   };
+
+   class OPTRE_m1015_mule_ins: O_Truck_02_transport_F
+   {
+      armor = 95;               // M1015 Mule: 1 HEAT
+      armorStructural = 3;
+      damageResistance = 0.005;
+   };
+
+   class OPTRE_Scythe: AAA_System_01_base_F
+   {
+      armor = 95;               // Scythe: 1 HEAT
+      armorStructural = 2;
+      damageResistance = 0.004;
+   };
+
+   class OPTRE_Lance: AAA_System_01_base_F
+   {
+      armor = 95;               // Lance: 1 HEAT
+      armorStructural = 2;
+      damageResistance = 0.004;
+   };
+
+   class OPTRE_M313_Base: Tank_F
+   {
+      armor = 855;              // Elephant: 6 HEAT; tested 8/8 at 1140
+      armorStructural = 4;
+      damageResistance = 0.01;
+      crewExplosionProtection = 0.65;
+   };
+
    class DMNS_M511_Springbok_base;
 
-   class DMNS_M511_Springbok_APC : DMNS_M511_Springbok_base
+   class DMNS_M511_Springbok_APC: DMNS_M511_Springbok_base
    {
-      armor = 420;
+      armor = 285;              // Springbok: 3 HEAT
       armorStructural = 3;
       damageResistance = 0.005;
    };
 
-   class DMNS_M511_Springbok_MGS : DMNS_M511_Springbok_base
+   class DMNS_M511_Springbok_MGS: DMNS_M511_Springbok_base
    {
-      armor = 420;
+      armor = 285;              // Springbok: 3 HEAT
       armorStructural = 3;
       damageResistance = 0.005;
    };
 
-   class DMNS_M511_Springbok_AA : DMNS_M511_Springbok_base
+   class DMNS_M511_Springbok_AA: DMNS_M511_Springbok_base
    {
-      armor = 420;
+      armor = 285;              // Springbok: 3 HEAT
       armorStructural = 3;
       damageResistance = 0.005;
    };
 
-   class DMNS_M511_Springbok_IFV : DMNS_M511_Springbok_base
+   class DMNS_M511_Springbok_IFV: DMNS_M511_Springbok_base
    {
-      armor = 420;
+      armor = 285;              // Springbok: 3 HEAT
       armorStructural = 3;
       damageResistance = 0.005;
    };
-   
-   class B_MBT_01_cannon_F;
-   class DMNS_M808B : B_MBT_01_cannon_F
+
+   class DMNS_M808B: B_MBT_01_cannon_F
    {
-      armor = 360;
+      armor = 380;
       armorStructural = 3.25;
       damageResistance = 0.0055;
       crewExplosionProtection = 0.65;
