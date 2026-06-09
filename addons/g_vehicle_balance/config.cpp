1b0dfdbe7abfc62cc5d2b5d3c552819328250051
Ryan <ryan@mcnamee.it>
2026-06-09 16:59:59 -0500

    Vehicle balancing excluding planes complete

--- addons/g_vehicle_balance/config.cpp
+++ addons/g_vehicle_balance/config.cpp
@@ -22,7 +22,13 @@ class CfgPatches
          "DMNS_Vehicles_M511_Springbok",
          "DMNS_Vehicles_M808B",
          "DMNS_Armour",
-         "QAV_Merkava"
+         "QAV_Merkava",
+         "OPTRE_Vehicles_M494",
+         "OPTRE_Vehicles_Sabre",
+         "TKE_Ext_Core_V",
+         "tbd_d20_vanilla",
+         "Splits_Vehicles_Air_Pelican",
+         "DMNS_Shortsword_F"
       };
    };
 };
@@ -37,11 +43,18 @@ class CfgVehicles
 
    class Heli_Light_01_base_F;
    class Helicopter_Base_H;
+   class Car_F;
    class APC_Wheeled_01_base_F;
+   class I_APC_Tracked_03_base_F;
    class B_MBT_01_cannon_F;
    class O_Truck_02_transport_F;
    class AAA_System_01_base_F;
    class Tank_F;
+   class Plane_Base_F;
+   class Plane_Fighter_01_Base_F;
+   class B_Heli_Transport_01_F;
+   class StaticMortar;
+   class TKE_Ext_Heli_BASE;
 
    class OPTRE_falcon_base: Heli_Light_01_base_F
    {
@@ -66,7 +79,7 @@ class CfgVehicles
 
    class OPTRE_Pelican_F: Helicopter_Base_H
    {
-      armor = 245;              // Pelican: 4 HEAT; tested 5/5 at 305
+      armor = 195;              // Pelican: 4 HEAT; tested 5/5 at 245
       armorStructural = 3;
       damageResistance = 0.00555;
    };
@@ -131,17 +144,60 @@ class CfgVehicles
       crewExplosionProtection = 0.65;
    };
 
-   class qav_merkava_base: B_MBT_01_cannon_F
+   class OPTRE_M494: I_APC_Tracked_03_base_F
    {
-      armor = 570;              // Merkava: 3 HEAT
+      armor = 570;              // Oryx: 3 HEAT
       armorStructural = 3.25;
       damageResistance = 0.0055;
       crewExplosionProtection = 0.65;
    };
 
+   class OPTRE_Sabre_Base: Plane_Fighter_01_Base_F
+   {
+      armor = 570;              // Longsword / YSS-1000: 6 HEAT
+      armorStructural = 3;
+      damageResistance = 0.00555;
+   };
+
+   class qav_merkava_base: B_MBT_01_cannon_F
+   {
+      armor = 1150;             // Merkava: 3 HEAT; tested 2/2 at 770
+      armorStructural = 3.25;
+      damageResistance = 0.0055;
+      crewExplosionProtection = 0.65;
+
+      class HitPoints
+      {
+         class HitHull
+         {
+            armor = 5.4;
+         };
+
+         class HitEngine
+         {
+            armor = 2.4;
+         };
+
+         class HitFuel
+         {
+            armor = 1.8;
+         };
+
+         class HitLTrack
+         {
+            armor = -1950;
+         };
+
+         class HitRTrack
+         {
+            armor = -1950;
+         };
+      };
+   };
+
    class qav_merkava_mk2D: qav_merkava_base
    {
-      armor = 570;              // Merkava: 3 HEAT
+      armor = 1150;             // Merkava: 3 HEAT; tested 2/2 at 770
       armorStructural = 3.25;
       damageResistance = 0.0055;
       crewExplosionProtection = 0.65;
@@ -149,49 +205,447 @@ class CfgVehicles
 
    class qav_merkava_mk3b: qav_merkava_base
    {
-      armor = 570;              // Merkava: 3 HEAT
+      armor = 1150;             // Merkava: 3 HEAT; tested 2/2 at 770
       armorStructural = 3.25;
       damageResistance = 0.0055;
       crewExplosionProtection = 0.65;
    };
 
-   class DMNS_M511_Springbok_base;
+   class TKE_Ext_Bearcat_BASE: APC_Wheeled_01_base_F
+   {
+      armor = 285;              // TC-15 Bearcat: 1.5 HEAT
+      armorStructural = 3;
+      damageResistance = 0.005;
+      crewExplosionProtection = 0.65;
+   };
+
+   class TKE_Ext_Dragonfly_S: TKE_Ext_Heli_BASE
+   {
+      armor = 190;              // Dragonfly: 2 HEAT
+      armorStructural = 3;
+      damageResistance = 0.00555;
+   };
+
+   class TKE_Ext_Dragonfly_A: TKE_Ext_Dragonfly_S
+   {
+      armor = 190;              // Dragonfly: 2 HEAT
+      armorStructural = 3;
+      damageResistance = 0.00555;
+   };
+
+   class TKE_Ext_Dragonfly_T: TKE_Ext_Heli_BASE
+   {
+      armor = 190;              // Dragonfly / Locust: 2 HEAT
+      armorStructural = 3;
+      damageResistance = 0.00555;
+   };
+
+   class tbd_d20_vanilla: StaticMortar
+   {
+      armor = 95;               // D-20 M1955 vanilla: 1 HEAT
+      armorStructural = 2;
+      damageResistance = 0.004;
+   };
+
+   class Splits_Pelican_base: B_Heli_Transport_01_F
+   {
+      armor = 195;              // Foundries Pelican: 4 HEAT; tested 5/5 at 245
+      armorStructural = 3;
+      damageResistance = 0.00555;
+   };
+
+   class Dmns_Plane_01_base_F: Plane_Base_F
+   {
+      armor = 380;              // Shortsword: 4 HEAT
+      armorStructural = 3;
+      damageResistance = 0.00555;
+
+      class HitPoints
+      {
+         class HitHull
+         {
+            armor = 2;
+            explosionShielding = 3;
+            name = "HitHull";
+            passThrough = 1;
+            visual = "Hit_Hull";
+            radius = 0.3;
+            minimalHit = 0.05;
+            depends = "0";
+            material = -1;
+         };
+
+         class HitEngine
+         {
+            armor = 2;
+            explosionShielding = 3.5;
+            name = "HitEngine";
+            passThrough = 1;
+            visual = "Hit_Engine";
+            radius = 0.3;
+            minimalHit = 0.05;
+            depends = "0";
+         };
+
+         class HitAvionics
+         {
+            armor = 1.5;
+            explosionShielding = 3.5;
+            name = "HitAvionics";
+            passThrough = 0.5;
+            visual = "";
+            radius = 0.2;
+            minimalHit = 0.05;
+            depends = "0";
+         };
+
+         class HitFuel
+         {
+            armor = 2.25;
+            explosionShielding = 4;
+            name = "HitFuel";
+            passThrough = 0.8;
+            visual = "";
+            radius = 0.3;
+            minimalHit = 0.1;
+            depends = "0";
+         };
+
+         class HitFuel_Lead_Left
+         {
+            armor = 1;
+            explosionShielding = 3;
+            name = "HitFuel1_Leads";
+            passThrough = 0;
+            visual = "Hit_AileronL";
+            radius = 0.13;
+            minimalHit = 0.1;
+         };
+
+         class HitFuel_Lead_Right
+         {
+            armor = 1;
+            explosionShielding = 3;
+            name = "HitFuel2_leads";
+            passThrough = 0;
+            visual = "Hit_AileronR";
+            radius = 0.13;
+            minimalHit = 0.1;
+         };
+
+         class HitFuel_Left
+         {
+            armor = 1.25;
+            explosionShielding = 5;
+            name = "HitFuel1";
+            passThrough = 0.2;
+            visual = "Hit_Fuel2a";
+            radius = 0.2;
+            minimalHit = 0.1;
+            depends = "HitFuel_Lead_Left";
+         };
+
+         class HitFuel_Right
+         {
+            armor = 1.25;
+            explosionShielding = 5;
+            name = "HitFuel2";
+            passThrough = 0.2;
+            visual = "Hit_Fuel2b";
+            radius = 0.2;
+            minimalHit = 0.1;
+            depends = "HitFuel_Lead_Right";
+         };
+
+         class HitFuel2
+         {
+            armor = 2.25;       // Override inherited 999 aggregate fuel armor.
+            depends = "0";
+            name = "HitFuel2";
+            visual = "";
+            radius = 0.2;
+            minimalHit = 0.1;
+            explosionShielding = 1;
+            passThrough = 1;
+         };
+
+         class HitGlass1
+         {
+            armor = 1.2;
+            explosionShielding = 3;
+            name = "HitGlass1";
+            passThrough = 0;
+            visual = "glass1";
+            radius = 0.2;
+            minimalHit = 0.1;
+            depends = "0";
+         };
+
+         class HitGlass2
+         {
+            armor = 1.2;
+            explosionShielding = 3;
+            name = "HitGlass2";
+            passThrough = 0;
+            visual = "glass2";
+            radius = 0.2;
+            minimalHit = 0.1;
+         };
+
+         class HitGlass3
+         {
+            armor = 1.2;
+            explosionShielding = 3;
+            name = "HitGlass3";
+            passThrough = 0;
+            visual = "glass3";
+            radius = 0.2;
+            minimalHit = 0.1;
+         };
+
+         class HitLAileron_Link
+         {
+            armor = 1;
+            explosionShielding = 3.5;
+            name = "HitLAileron_Link";
+            passThrough = 0;
+            visual = "Hit_AileronL";
+            radius = 0.09;
+            minimalHit = 0.1;
+            depends = "0";
+         };
+
+         class HitRAileron_Link
+         {
+            armor = 1;
+            explosionShielding = 3.5;
+            name = "HitRAileron_Link";
+            passThrough = 0;
+            visual = "Hit_AileronR";
+            radius = 0.09;
+            minimalHit = 0.1;
+            depends = "0";
+         };
+
+         class HitLAileron
+         {
+            armor = 1;
+            explosionShielding = 3;
+            name = "HitLAileron";
+            passThrough = 0.3;
+            visual = "Hit_AileronL";
+            radius = 0.2;
+            minimalHit = 0.1;
+            depends = "HitLAileron_Link";
+         };
+
+         class HitRAileron
+         {
+            armor = 1;
+            explosionShielding = 3;
+            name = "HitRAileron";
+            passThrough = 0.3;
+            visual = "Hit_AileronR";
+            radius = 0.2;
+            minimalHit = 0.1;
+            depends = "HitRAileron_Link";
+         };
+
+         class HitLCRudder
+         {
+            armor = 1;
+            explosionShielding = 3;
+            name = "HitLCRudder";
+            passThrough = 0.3;
+            visual = "Hit_RudderC";
+            radius = 0.2;
+            minimalHit = 0.1;
+            depends = "0";
+         };
+
+         class HitLCElevator
+         {
+            armor = 1;
+            explosionShielding = 3;
+            name = "HitLCElevator";
+            passThrough = 0.3;
+            visual = "Hit_ElevatorL";
+            radius = 0.2;
+            minimalHit = 0.1;
+            depends = "0";
+         };
+
+         class HitRElevator
+         {
+            armor = 1;
+            explosionShielding = 3;
+            name = "HitRElevator";
+            passThrough = 0.3;
+            visual = "Hit_ElevatorR";
+            radius = 0.2;
+            minimalHit = 0.1;
+            depends = "0";
+         };
+      };
+   };
+
+   class DMNS_Shortsword_01_F: Dmns_Plane_01_base_F
+   {
+      armor = 380;              // Shortsword: 4 HEAT
+      armorStructural = 3;
+      damageResistance = 0.00555;
+
+      class HitPoints
+      {
+         class HitHull
+         {
+            armor = 2;
+            explosionShielding = 1;
+            name = "HitHull";
+            passThrough = 1;
+            visual = "Hit_Hull";
+            radius = 0.5;
+            minimalHit = 0.01;
+            depends = "0";
+            material = -1;
+         };
+
+         class HitEngine
+         {
+            armor = 2;
+            explosionShielding = 1;
+            name = "HitEngine";
+            passThrough = 1;
+            visual = "Hit_Engine";
+            radius = 0.5;
+            minimalHit = 0.01;
+            depends = "0";
+         };
+
+         class HitAvionics
+         {
+            armor = 1.5;
+            explosionShielding = 1;
+            name = "HitAvionics";
+            passThrough = 0.5;
+            visual = "";
+            radius = 0.3;
+            minimalHit = 0.01;
+            depends = "0";
+         };
+
+         class HitFuel
+         {
+            armor = 2.25;
+            explosionShielding = 1;
+            name = "HitFuel";
+            passThrough = 1;
+            visual = "";
+            radius = 0.5;
+            minimalHit = 0.01;
+            depends = "0";
+         };
+
+         class HitFuel_Lead_Left
+         {
+            armor = 1;
+            explosionShielding = 1;
+            name = "HitFuel1_Leads";
+            passThrough = 0.5;
+            visual = "Hit_AileronL";
+            radius = 0.2;
+            minimalHit = 0.01;
+         };
+
+         class HitFuel_Lead_Right
+         {
+            armor = 1;
+            explosionShielding = 1;
+            name = "HitFuel2_leads";
+            passThrough = 0.5;
+            visual = "Hit_AileronR";
+            radius = 0.2;
+            minimalHit = 0.01;
+         };
+
+         class HitFuel_Left
+         {
+            armor = 1.25;
+            explosionShielding = 1;
+            name = "HitFuel1";
+            passThrough = 0.5;
+            visual = "Hit_Fuel2a";
+            radius = 0.3;
+            minimalHit = 0.01;
+            depends = "HitFuel_Lead_Left";
+         };
+
+         class HitFuel_Right
+         {
+            armor = 1.25;
+            explosionShielding = 1;
+            name = "HitFuel2";
+            passThrough = 0.5;
+            visual = "Hit_Fuel2b";
+            radius = 0.3;
+            minimalHit = 0.01;
+            depends = "HitFuel_Lead_Right";
+         };
+
+         class HitFuel2
+         {
+            armor = 2.25;
+            depends = "0";
+            name = "HitFuel2";
+            visual = "";
+            radius = 0.3;
+            minimalHit = 0.01;
+            explosionShielding = 1;
+            passThrough = 1;
+         };
+      };
+   };
+
+   class DMNS_M511_Springbok_base: Car_F
+   {
+      armor = 760;              // Springbok: 3 HEAT; tested 3/4 at 855
+      armorStructural = 3;
+      damageResistance = 0.005;
+   };
 
    class DMNS_M511_Springbok_APC: DMNS_M511_Springbok_base
    {
-      armor = 285;              // Springbok: 3 HEAT
+      armor = 760;              // Springbok: 3 HEAT; tested 3/4 at 855
       armorStructural = 3;
       damageResistance = 0.005;
    };
 
    class DMNS_M511_Springbok_MGS: DMNS_M511_Springbok_base
    {
-      armor = 285;              // Springbok: 3 HEAT
+      armor = 760;              // Springbok: 3 HEAT; tested 3/4 at 855
       armorStructural = 3;
       damageResistance = 0.005;
    };
 
    class DMNS_M511_Springbok_AA: DMNS_M511_Springbok_base
    {
-      armor = 285;              // Springbok: 3 HEAT
+      armor = 760;              // Springbok: 3 HEAT; tested 3/4 at 855
       armorStructural = 3;
       damageResistance = 0.005;
    };
 
    class DMNS_M511_Springbok_IFV: DMNS_M511_Springbok_base
    {
-      armor = 285;              // Springbok: 3 HEAT
+      armor = 760;              // Springbok: 3 HEAT; tested 3/4 at 855
       armorStructural = 3;
       damageResistance = 0.005;
    };
 
    class DMNS_M808B: B_MBT_01_cannon_F
    {
-      armor = 380;
+      armor = 760;              // Foundries Scorpion: 4 HEAT
       armorStructural = 3.25;
       damageResistance = 0.0055;
       crewExplosionProtection = 0.65;
    };
 };
-
-*test*
