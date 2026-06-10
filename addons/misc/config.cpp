a06cc47cd93a4da323a80c39ba8fd02c11fb8a2a
Ryan <ryan@mcnamee.it>
2026-06-09 21:03:53 -0500

    Stamina Tweaks

---
+++ addons/misc/config.cpp
@@ -0,0 +1,21 @@
+class CfgPatches
+{
+	class STB_Misc
+	{
+ 		units[] = {};	
+ 		weapons[] = {};
+ 		requiredAddons[] = {};	
+		author="Weber";
+ 	};
+};
+
+class CfgMovesFatigue
+{
+	staminaDuration = 180;
+	staminaRestoration = 30;
+	staminaCooldown = 15;
+	aimPrecisionSpeedCoef = 5;
+	terrainDrainSprint = -1;
+	terrainDrainRun = 0;
+	terrainSpeedCoef = 0.9;
+};
