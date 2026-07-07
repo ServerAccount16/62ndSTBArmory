49e1627f8aea6c085a67ff815e13ad6fdeef7fc0
Ryan <ryan@mcnamee.it>
2026-07-07 14:29:48 -0500

    M96 typicalspeed increase

--- addons/62stb_balance_ammo/config.cpp
+++ addons/62stb_balance_ammo/config.cpp
@@ -26,7 +26,19 @@ class CfgAmmo
       hit = 142.5;
       indirectHit = 30;
       indirectHitRange = 4.95;
+      maxSpeed = 200;
+      typicalSpeed = 200;
+      initTime = 0;
       submunitionAmmo = "ammo_Penetrator_Titan_AT";
       submunitionInitSpeed = 1000;
+   };
+};
+
+class CfgMagazines
+{
+   class RPG32_HE_F;
+   class DMNS_M96_HEAT: RPG32_HE_F
+   {
+      initSpeed = 200;
    };
 };
